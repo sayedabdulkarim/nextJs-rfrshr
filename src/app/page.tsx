8bd4d6d9f63572a197b467f667655dbe913ffcf1
Sayed Abdul <sakarim9124@gmail.com>
2025-10-17 23:28:47 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <Link href="/about">Go to About Page</Link>
+      <Link href="/blog">Go to Blog Page</Link>
+    </div>
+  );
 };
 
 export default Home;
